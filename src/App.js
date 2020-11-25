5162ea5f0745d8e564b4ab6905ed151a66d8a126
migasubi <yuri.ossipov@gmail.com>
2020-11-25 00:14:21 -0500

    header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component'
+
 
 const HatsPage = () => (
   <div>
@@ -14,10 +16,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>    
-     
+      <Header/>
+     <switch> 
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
-   
+     </switch>
     </div>
   );
 }
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.styles.scss';
+
+
+
+const Header = () => (
+  <div className='header'>
+    
+    <Link className='logo-container' to="/">
+       <Logo className='logo'/>
+    </Link>
+
+    <div className='options'>
+      <Link className='options' to='/shop'>
+        Shop
+      </Link>
+      <Link className='options' to='/contact'>
+        Contact
+      </Link>
+    </div>
+
+  </div>
+)
+
+
+export default Header;
